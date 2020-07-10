5c08e6dd7f105cab0bbe527cdfad9191cf153903
joaopaludo <joao.ps@live.com>
2020-07-10 13:47:33 -0300

    Update config repo name

--- larbs.sh
+++ larbs.sh
@@ -14,7 +14,7 @@ while getopts ":a:r:b:p:h" o; do case "${o}" in
 	*) printf "Invalid option: -%s\\n" "$OPTARG" && exit ;;
 esac done
 
-[ -z "$dotfilesrepo" ] && dotfilesrepo="https://github.com/JoaoAPS/configs.git"
+[ -z "$dotfilesrepo" ] && dotfilesrepo="https://github.com/JoaoAPS/dotfiles.git"
 [ -z "$progsfile" ] && progsfile="https://raw.githubusercontent.com/JoaoAPS/LARBS/master/progs.csv"
 [ -z "$aurhelper" ] && aurhelper="yay"
 [ -z "$repobranch" ] && repobranch="master"
