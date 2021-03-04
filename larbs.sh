9690dbabba22315a2637da9930b78cd3ed99e4fd
João Paludo <jas.paludo@gmail.com>
2021-03-03 21:00:32 -0300

    Change installation script to adapt to no-os instalation

--- larbs.sh
+++ larbs.sh
@@ -17,7 +17,7 @@ esac done
 [ -z "$dotfilesrepo" ] && dotfilesrepo="https://github.com/JoaoAPS/dotfiles.git"
 [ -z "$progsfile" ] && progsfile="https://raw.githubusercontent.com/JoaoAPS/LARBS/master/progs.csv"
 [ -z "$aurhelper" ] && aurhelper="yay"
-[ -z "$repobranch" ] && repobranch="main"
+[ -z "$repobranch" ] && repobranch="no_os"
 
 ### FUNCTIONS ###
 
@@ -38,41 +38,20 @@ error() { clear; printf "ERROR:\\n%s\\n" "$1"; exit;}
 welcomemsg() { \
 	dialog --title "Welcome!" --msgbox "Welcome to Luke's Auto-Rice Bootstrapping Script!\\n\\nThis script will automatically install a fully-featured Linux desktop, which I use as my main machine.\\n\\n-Luke" 10 60
 
+	dialog --colors --title "Important Note!" --yes-label "I have an os!" --no-label "Cancel!" --yesno "You are doing a no-os installation, which means an operating system must already be installed." 8 70 || clear; exit;
+	
 	dialog --colors --title "Important Note!" --yes-label "All ready!" --no-label "Return..." --yesno "Be sure the computer you are using has current pacman updates and refreshed Arch keyrings.\\n\\nIf it does not, the installation of some programs might fail." 8 70
 }
 
-getuserandpass() { \
-	# Prompts user for new username an password.
-	name=$(dialog --inputbox "First, please enter a name for the user account." 10 60 3>&1 1>&2 2>&3 3>&1) || exit
-	while ! echo "$name" | grep "^[a-z_][a-z0-9_-]*$" >/dev/null 2>&1; do
-		name=$(dialog --no-cancel --inputbox "Username not valid. Give a username beginning with a letter, with only lowercase letters, - or _." 10 60 3>&1 1>&2 2>&3 3>&1)
-	done
-	pass1=$(dialog --no-cancel --passwordbox "Enter a password for that user." 10 60 3>&1 1>&2 2>&3 3>&1)
-	pass2=$(dialog --no-cancel --passwordbox "Retype password." 10 60 3>&1 1>&2 2>&3 3>&1)
-	while ! [ "$pass1" = "$pass2" ]; do
-		unset pass2
-		pass1=$(dialog --no-cancel --passwordbox "Passwords do not match.\\n\\nEnter password again." 10 60 3>&1 1>&2 2>&3 3>&1)
-		pass2=$(dialog --no-cancel --passwordbox "Retype password." 10 60 3>&1 1>&2 2>&3 3>&1)
-	done ;
-}
 
 usercheck() { \
-	! (id -u "$name" >/dev/null) 2>&1 ||
-	dialog --colors --title "WARNING!" --yes-label "CONTINUE" --no-label "No wait..." --yesno "The user \`$name\` already exists on this system. LARBS can install for a user already existing, but it will \\Zboverwrite\\Zn any conflicting settings/dotfiles on the user account.\\n\\nLARBS will \\Zbnot\\Zn overwrite your user files, documents, videos, etc., so don't worry about that, but only click <CONTINUE> if you don't mind your settings being overwritten.\\n\\nNote also that LARBS will change $name's password to the one you just gave." 14 70
+	(id -u "$USER" >/dev/null) 2>&1 ||
+	dialog --colors --title "WARNING!" --msgbox "You must be logged in to perform a no-os installation. Create a user if needed."  6 50 &&
+	clear; exit;
 }
 
 preinstallmsg() { \
 	dialog --title "Let's get this party started!" --yes-label "Let's go!" --no-label "No, nevermind!" --yesno "The rest of the installation will now be totally automated, so you can sit back and relax.\\n\\nIt will take some time, but when done, you can relax even more with your complete system.\\n\\nNow just press <Let's go!> and the system will begin installation!" 13 60 || { clear; exit; }
-}
-
-adduserandpass() { \
-	# Adds user `$name` with password $pass1.
-	dialog --infobox "Adding user \"$name\"..." 4 50
-	useradd -m -g wheel -s /bin/bash "$name" >/dev/null 2>&1 ||
-	usermod -a -G wheel "$name" && mkdir -p /home/"$name" && chown "$name":wheel /home/"$name"
-	repodir="/home/$name/.local/src"; mkdir -p "$repodir"; chown -R "$name":wheel $(dirname "$repodir")
-	echo "$name:$pass1" | chpasswd
-	unset pass1 pass2 ;
 }
 
 refreshkeys() { \
@@ -91,9 +70,9 @@ manualinstall() { # Installs $1 manually if not installed. Used only for AUR hel
 	cd /tmp || exit
 	rm -rf /tmp/"$1"*
 	curl -sO https://aur.archlinux.org/cgit/aur.git/snapshot/"$1".tar.gz &&
-	sudo -u "$name" tar -xvf "$1".tar.gz >/dev/null 2>&1 &&
+	sudo -u "$USER" tar -xvf "$1".tar.gz >/dev/null 2>&1 &&
 	cd "$1" &&
-	sudo -u "$name" makepkg --noconfirm -si >/dev/null 2>&1
+	sudo -u "$USER" makepkg --noconfirm -si >/dev/null 2>&1
 	cd /tmp || return) ;
 }
 
@@ -106,7 +85,7 @@ gitmakeinstall() {
 	progname="$(basename "$1" .git)"
 	dir="$repodir/$progname"
 	dialog --title "LARBS Installation" --infobox "Installing \`$progname\` ($n of $total) via \`git\` and \`make\`. $(basename "$1") $2" 5 70
-	sudo -u "$name" git clone --depth 1 "$1" "$dir" >/dev/null 2>&1 || { cd "$dir" || return ; sudo -u "$name" git pull --force origin master;}
+	sudo -u "$USER" git clone --depth 1 "$1" "$dir" >/dev/null 2>&1 || { cd "$dir" || return ; sudo -u "$USER" git pull --force origin master;}
 	cd "$dir" || exit
 	make >/dev/null 2>&1
 	make install >/dev/null 2>&1
@@ -116,7 +95,7 @@ gitmakeinstall() {
 aurinstall() { \
 	dialog --title "LARBS Installation" --infobox "Installing \`$1\` ($n of $total) from the AUR. $1 $2" 5 70
 	echo "$aurinstalled" | grep "^$1$" >/dev/null 2>&1 && return
-	sudo -u "$name" $aurhelper -S --noconfirm "$1" >/dev/null 2>&1
+	sudo -u "$USER" $aurhelper -S --noconfirm "$1" >/dev/null 2>&1
 }
 
 pipinstall() { \
@@ -146,9 +125,9 @@ putgitrepo() { # Downloads a gitrepo $1 and places the files in $2 only overwrit
 	[ -z "$3" ] && branch="master" || branch="$repobranch"
 	dir=$(mktemp -d)
 	[ ! -d "$2" ] && mkdir -p "$2"
-	chown -R "$name":wheel "$dir" "$2"
-	sudo -u "$name" git clone --recursive -b "$branch" --depth 1 "$1" "$dir" >/dev/null 2>&1
-	sudo -u "$name" cp -rfT "$dir" "$2"
+	chown -R "$USER":wheel "$dir" "$2"
+	sudo -u "$USER" git clone --recursive -b "$branch" --depth 1 "$1" "$dir" >/dev/null 2>&1
+	sudo -u "$USER" cp -rfT "$dir" "$2"
 }
 
 systembeepoff() { dialog --infobox "Getting rid of that retarded error beep sound..." 10 50
@@ -171,18 +150,16 @@ installpkg dialog || error "Are you sure you're running this as the root user an
 # Welcome user and pick dotfiles.
 welcomemsg || error "User exited."
 
-# Get and verify username and password.
-getuserandpass || error "User exited."
+# # Get and verify username and password.
+# getuserandpass || error "User exited."
 
-# Give warning if user already exists.
-usercheck || error "User exited."
+# Give warning if user does not exists.
+usercheck
 
 # Last chance for user to back out before install.
 preinstallmsg || error "User exited."
 
 ### The rest of the script requires no user input.
-
-adduserandpass || error "Error adding username and/or password."
 
 # Refresh Arch keyrings.
 refreshkeys || error "Error automatically refreshing Arch keyring. Consider doing so manually."
@@ -221,34 +198,27 @@ ntpdate 0.us.pool.ntp.org >/dev/null 2>&1
 installationloop
 
 dialog --title "LARBS Installation" --infobox "Finally, installing \`libxft-bgra\` to enable color emoji in suckless software without crashes." 5 70
-yes | sudo -u "$name" $aurhelper -S libxft-bgra >/dev/null 2>&1
-
-# # Install the dotfiles in the user's home directory
-# putgitrepo "$dotfilesrepo" "/home/$name" "$repobranch"
-# rm -f "/home/$name/README.md" "/home/$name/LICENSE" "/home/$name/FUNDING.yml"
-# # make git ignore deleted LICENSE & README.md files
-# git update-index --assume-unchanged "/home/$name/README.md"
-# git update-index --assume-unchanged "/home/$name/LICENSE"
+yes | sudo -u "$USER" $aurhelper -S libxft-bgra >/dev/null 2>&1
 
 # Install the dotfiles in the user's home directory (as in https://www.atlassian.com/git/tutorials/dotfiles)
-alias config='/usr/bin/git --git-dir=/home/$name/.dotfiles/ --work-tree=home/$name'
-echo -e "alias config='/usr/bin/git --git-dir=/home/$name/.dotfiles/ --work-tree=/home/$name'\n" | tee .bashrc .zrc > /dev/null
-git clone --bare $dotfilesrepo /home/$name/.dotfiles
+alias config='/usr/bin/git --git-dir=$HOME/.dotfiles/ --work-tree=home/$USER'
+echo -e "# Access dotfiles git repo\nalias config='/usr/bin/git --git-dir=$HOME/.dotfiles/ --work-tree=$HOME'\n" | tee $HOME/.bashrc $HOME/.zrc > /dev/null
+git clone --bare $dotfilesrepo $HOME/.dotfiles
 config checkout -f > 2>&1
 config config --local status.showUntrackedFiles no
 
-rm -f "/home/$name/README.md" "/home/$name/LICENSE" "/home/$name/FUNDING.yml" 2> /dev/null
+rm -f "$HOME/README.md" "$HOME/LICENSE" "$HOME/FUNDING.yml" 2> /dev/null
 # make git ignore deleted LICENSE & README.md files
-config update-index --assume-unchanged "/home/$name/README.md"
-config update-index --assume-unchanged "/home/$name/LICENSE"
+config update-index --assume-unchanged "$HOME/README.md"
+config update-index --assume-unchanged "$HOME/LICENSE"
 
 
 # Most important command! Get rid of the beep!
 systembeepoff
 
 # Make zsh the default shell for the user.
-chsh -s /bin/zsh $name >/dev/null 2>&1
-sudo -u "$name" mkdir -p "/home/$name/.cache/zsh/"
+chsh -s /bin/zsh $USER >/dev/null 2>&1
+sudo -u "$USER" mkdir -p "$HOME/.cache/zsh/"
 
 # dbus UUID must be generated for Artix runit.
 dbus-uuidgen > /var/lib/dbus/machine-id
@@ -257,7 +227,7 @@ dbus-uuidgen > /var/lib/dbus/machine-id
 grep -q "laptop-updates.brave.com" /etc/hosts || echo "0.0.0.0 laptop-updates.brave.com" >> /etc/hosts
 
 # Start/restart PulseAudio.
-killall pulseaudio; sudo -u "$name" pulseaudio --start
+killall pulseaudio; sudo -u "$USER" pulseaudio --start
 
 # This line, overwriting the `newperms` command above will allow the user to run
 # serveral important commands, `shutdown`, `reboot`, updating, etc. without a password.
